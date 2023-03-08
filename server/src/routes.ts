ce8e5b916605ce1e80fed53fed90df404a001e1f
João Macêdo <joaomacedx@gmail.com>
2023-03-08 00:01:14 -0300

    feat: create migrations and database

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,7 @@
-import express from 'express';
+import Express from 'express';
 
 
-const routes = express.Router();
+const routes = Express.Router();
 
 routes.get('/', (request, response)=>{
    response.send({message: "Hello world!"});
